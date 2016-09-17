3cf28d7b41d203373ec8dacd3ee5cce2d03ccc8c
Kevin Vance <kavance2@gmail.com>
2016-09-17 02:35:03 +0000

    intitial commit for file

---
+++ pythonclassroom/namever.py
@@ -0,0 +1,22 @@
+name = "eric"
+print name + " how was your day?"
+
+name = "kevin vance"
+print name.title()
+
+print name.upper()
+
+print name.lower()
+
+print 'Kevin Vance "How you live is a choice, \n make the right choice"'
+
+print name.title() + ' "How you live life is a choice, \n make the right choice" '
+    
+new_qoute = name.title() + ' "How you live life is a choice, \n make the right choice" '
+print new_qoute
+
+name = ' Kevin Vance\n the \tgreat'
+print name
+print name.rstrip()
+print name.lstrip()
+print name.strip()
