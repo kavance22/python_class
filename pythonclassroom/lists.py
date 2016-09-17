77800e7231b92f6f5aea015874505312ab4e656c
Kevin Vance <kavance2@gmail.com>
2016-09-17 18:10:23 +0000

    first list file commit

---
+++ pythonclassroom/lists.py
@@ -0,0 +1,10 @@
+friends_names = ["David", "Wilson", "Paul", "Edward"]
+print friends_names[0]
+print friends_names[1]
+print friends_names[2]
+print friends_names[3]
+
+print friends_names[0] + " Thanks for being here"
+print friends_names[1] + " Thanks for being here"
+print friends_names[2] + " Thanks for being here"
+print friends_names[3] + " Thanks for being here"
